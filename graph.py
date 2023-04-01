8e483bfafc8ef84ae2483e0417452d32c91fb969
morveusz93 <morveusz93@gmail.com>
2023-04-01 17:34:54 +0200

    refactor

--- graph.py
+++ graph.py
@@ -18,27 +18,27 @@ class Graph:
     def get_active_node(self) -> Optional['Node']:
         return [n for n in self.nodes if n.active][0] if self.any_active_node else None
 
-    def create_node(self, pos: 'tuple[int, int]'):
+    def create_node(self, pos: 'tuple[int, int]') -> None:
         new_node = Node(pos)
         self.nodes.append(new_node)
 
-    def create_edge(self, start: 'Node', end: 'Node'):
+    def create_edge(self, start: 'Node', end: 'Node') -> None:
         new_edge = Edge(start, end)
         self.edges.append(new_edge)
 
-    def check_collide(self, pos: 'tuple[int, int]'):
+    def check_collide(self, pos: 'tuple[int, int]') -> 'Optional[Node]':
         # if the circles are overlapped, we want to remove the lateset one (appened to list later)
         for node in self.nodes[::-1]:
             if node.collidepoint(*pos, self.surface):
                 return node
 
-    def connect_nodes(self, end_node):
+    def connect_nodes(self, end_node) -> None:
         start_node = self.get_active_node()
         if start_node != end_node:
             self.create_edge(start=start_node, end=end_node)
         start_node.toggle_active()
 
-    def draw(self):
+    def draw(self) -> None:
         for obj in self.nodes + self.edges:
             obj.draw(self.surface)
 
@@ -52,7 +52,7 @@ class Node:
         rect = self.draw(surface)
         return rect.collidepoint(x, y)
  
-    def toggle_active(self):
+    def toggle_active(self) -> None:
         self.active = not self.active
  
     def draw(self, surface: Surface) -> Rect:
@@ -72,26 +72,26 @@ class Edge:
         self.calculate_start_pos()
         self.calculate_end_pos()
 
-    def calculate_rotation(self):
+    def calculate_rotation(self) -> None:
         start_pos = self.start_node.pos
         end_pos = self.end_node.pos
         self.rotation = math.degrees(math.atan2(start_pos[1] - end_pos[1], end_pos[0] - start_pos[0])) + 90
 
-    def calculate_start_pos(self):
+    def calculate_start_pos(self) -> None:
         start_pos = self.start_node.pos
         self.start_pos = (
             start_pos[0] + NODE_RADIUS * math.sin(math.radians(self.rotation)), 
             start_pos[1] + NODE_RADIUS * math.cos(math.radians(self.rotation))
         )
 
-    def calculate_end_pos(self):
+    def calculate_end_pos(self) -> None:
         end_pos = self.end_node.pos
         self.end_pos = (
             end_pos[0] - (NODE_RADIUS + EDGE_ARROW_LENGTH) * math.sin(math.radians(self.rotation)),
             end_pos[1] - (NODE_RADIUS + EDGE_ARROW_LENGTH) * math.cos(math.radians(self.rotation))
         )
 
-    def draw(self, surface: Surface):
+    def draw(self, surface: Surface) -> None:
         color = EDGE_COLOR
         if self.active:
             color = EDGE_ACTIVE_COLOR
